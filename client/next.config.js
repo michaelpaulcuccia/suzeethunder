2c8c93d7aff694a1f88e576d5a2bb3985e3fad99
Michael Cuccia <michael.cuccia@egplusww.com>
2022-02-01 17:08:39 -0500

    set up nextjs and styledcomponents

---
+++ client/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  experimental: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+}
+
+//https://nextjs.org/docs/advanced-features/compiler#styled-components
